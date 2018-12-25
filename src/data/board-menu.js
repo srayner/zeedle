5ee05aaded922af7feb629b00ee0b10d93a0dbd6
Steve Rayner <srayner02@gmail.com>
2018-12-25 11:27:03 +0000

    Added board menu config.

---
+++ src/data/board-menu.js
@@ -0,0 +1,18 @@
+export default {
+  id: "BOARD_ACTIONS_MENU",
+  title: "Board actions",
+  width: "260px",
+  position: {
+    top: "100px",
+    right: "1px"
+  },
+  items: [
+    [
+      { id: "STAR", caption: "Star Board" },
+      { id: "BACKGROUND", caption: "Change Background" }
+    ],
+    [{ id: "DELETE", caption: "Delete Board" }]
+  ],
+  onItemClick: null,
+  onClose: null
+};
