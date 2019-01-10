d073b802839a9784d6a8e8eb6f4f37b0c596af23
Steve Rayner <srayner02@gmail.com>
2019-01-10 08:42:18 +0000

    started to style profile header

--- src/components/profile/profile-header.js
+++ src/components/profile/profile-header.js
@@ -2,14 +2,36 @@ import React from "react";
 import styled from "styled-components";
 import Avatar from "../ui/avatar";
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: white;
+  margin: 0 auto;
+  width: 500px;
+  display: flex;
+`;
+
+const DisplayName = styled.h2`
+  margin: 0 0 3px 0;
+  padding: 0;
+  color: black;
+`;
+
+const Username = styled.h3`
+  margin: 0;
+  padding: 0;
+  color: #777;
+  font-size: 14px;
+  font-weight: 400;
+`;
+
 const ProfileHeader = props => {
   const { displayName, username, initials } = props;
   return (
     <Container>
       <Avatar>{initials}</Avatar>
-      <h2>{displayName}</h2>
-      <h3>{username}</h3>
+      <div>
+        <DisplayName>{displayName}</DisplayName>
+        <Username>@{username}</Username>
+      </div>
     </Container>
   );
 };
